3d6e068ea30238285114c514f626ec6abdfe20f1
Grant McDade <grant.mcdade@gmail.com>
2017-08-13 17:16:47 +0200

    Changed firebase init

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ along with Shared Load.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
 import store from './store'
 import firebase from 'firebase'
@@ -25,6 +25,7 @@ import Vuetify from 'vuetify'
 import injector from 'vue-inject'
 import '../node_modules/vuetify/dist/vuetify.min.css'
 import './service'
+import config from '../temp-settings'
 
 Vue.use(VueFire)
 Vue.use(injector)
@@ -39,10 +40,6 @@ function setUser(user) {
   }
 }
 
-firebase.auth().onAuthStateChanged(u => {
-  setUser(u)
-})
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -50,7 +47,20 @@ const app = new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  fb: null,
+  render: h => h(App),
+  created() {
+    this.fb = firebase.initializeApp(config.firebase)
+
+    firebase.auth().onAuthStateChanged(u => {
+      const user = {
+        id: u.uid,
+        name: u.displayName || u.name || u.email,
+        email: u.email
+      }
+      setUser(user)
+    })
+  }
 })
 
 export {
--- src/service.js
+++ src/service.js
@@ -1,17 +1,6 @@
 import injector from 'vue-inject'
-import firebase from 'firebase'
 import GroupService from './services/groups'
 import UserService from './services/users'
 
-import config from '../temp-settings'
-
-// Initialize Firebase
-const firebaseApp = firebase.initializeApp(config.firebase)
-
-injector.factory('db', firebase.database)
 injector.service('GroupService', GroupService)
 injector.service('UserService', UserService)
-
-export {
-  firebaseApp
-}
